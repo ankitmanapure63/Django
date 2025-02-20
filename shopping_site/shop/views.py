ae06d47b7d1d5371fbac867ea4e24a18f3e74ad9
Ankit Manapure <ankitmanapure63@gmail.com>
2025-02-20 20:26:43 +0530

    Fixed_code

--- shopping_site/shop/views.py
+++ shopping_site/shop/views.py
@@ -21,25 +21,24 @@ def create_hospital_record(request):
     )
     return HttpResponse(f"Created Hospital Record: {new_record.fname} {new_record.lname}, Age: {new_record.age}, Salary: {new_record.salary}")
 
-create dynamic data
+# create dynamic data
 
 
 
+# def add_data(request):
+#     if request.method == "POST":
+#         fname = request.POST.get('fname', None)
+#         lname = request.POST.get('lname', None)
+#         age = request.POST.get('age', None)
+#         salary = request.POST.get('salary', None)
 
-def add_data(request):
-    if request.method == "POST":
-        fname = request.POST.get('fname', None)
-        lname = request.POST.get('lname', None)
-        age = request.POST.get('age', None)
-        salary = request.POST.get('salary', None)
+#         if fname and lname and age and salary:
+#             data = Hospital.objects.create(fname=fname, lname=lname, age=age, salary=salary)
+#             data.save()
+#             return HttpResponse(f"Data added successfully: {fname} {lname}, Age: {age}, Salary: {salary}")
+#         else:
+#             return HttpResponse("Error: All fields are required.")
 
-        if fname and lname and age and salary:
-            data = Hospital.objects.create(fname=fname, lname=lname, age=age, salary=salary)
-            data.save()
-            return HttpResponse(f"Data added successfully: {fname} {lname}, Age: {age}, Salary: {salary}")
-        else:
-            return HttpResponse("Error: All fields are required.")
-
-    return HttpResponse("Please submit the form with all required fields.")
+#     return HttpResponse("Please submit the form with all required fields.")
 
 
